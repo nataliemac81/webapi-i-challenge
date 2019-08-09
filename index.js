8c580247e8f0b62190f8025a0a1eafbf4355f87d
Natalie McCroy <nataliemccroy@gmail.com>
2019-08-08 21:43:00 -0500

    get /users endpoint working

--- index.js
+++ index.js
@@ -6,8 +6,34 @@ const express = require('express')
 // create the server
 const server = express()
 
+// require the db file
+const db = require('./data/db')
+
+
 server.get('/', (req, res) => {
   res.send('Hello from my Express App!')
 })
+
+// GET /users
+
+server.get('/users', (req, res) => {
+	db.find()
+	.then (users => {
+		res.json(users)
+	})
+	.catch (error => {
+		res.status(500).json({
+			error: error
+		})
+	})
+})
+
+
+// POST /users
+server.post('/users', (req, res) => {
+	
+})
+
+
 
 server.listen(4000, () => console.log('API running on port 4000'))
