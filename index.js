bbc12cb8f9b6538437858d9acd821b95e3b4c330
Natalie McCroy <nataliemccroy@gmail.com>
2019-08-08 20:29:02 -0500

    server up and running on port 4000

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // implement your API here
+
+// require the express module
+const express = require('express')
+
+// create the server
+const server = express()
+
+server.get('/', (req, res) => {
+  res.send('Hello from my Express App!')
+})
+
+server.listen(4000, () => console.log('API running on port 4000'))
